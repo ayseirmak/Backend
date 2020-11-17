0788bbe2f3ac893faa6c54c474a71eef5bb8ceff
MURATHAN <murathankose@icloud.com>
2020-11-17 14:03:57 +0300

    heroku v0.04

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -3515,7 +3515,7 @@ const Login = () => {
     className: "mb-3 mt-3"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(StyledAnchorTag, {
     className: "btn btn-outline-dark alert-dismissible",
-    href: "https://tatli-sozluk.herokuapp.com/oauth2/authorize/google?redirect_uri=https://tatli-sozluk.herokuapp.com/auth"
+    href: "https://tatli-sozluk.herokuapp.com/oauth2/authorize/google?redirect_uri=https://tatli-sozluk.herokuapp.com/#/auth"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_fortawesome_react_fontawesome__WEBPACK_IMPORTED_MODULE_8__["FontAwesomeIcon"], {
     icon: _fortawesome_free_brands_svg_icons__WEBPACK_IMPORTED_MODULE_9__["faGoogle"],
     style: {
