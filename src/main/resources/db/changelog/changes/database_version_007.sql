561cc37aa6a9f263265398fbafa4145d2a5e1f23
MURATHAN <murathankose@icloud.com>
2020-11-18 17:06:05 +0300

    Bug fix

--- src/main/resources/db/changelog/changes/database_version_007.sql
+++ src/main/resources/db/changelog/changes/database_version_007.sql
@@ -49,7 +49,7 @@ create table if not exists "user_roles"
 create table if not exists "content_like"
 (
     user_id    int         not null references cloud_users (id),
-    content_id int         not null references cloud_content (id),
+    content_id int         not null references cloud_content (id)
 );
 
 create table if not exists "content_dislike"
