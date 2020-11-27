2c3baee7368206b1ab93f69c9da28d1a7cb194f6
MURATHAN <murathankose@icloud.com>
2020-11-27 16:23:07 +0300

    Added services and refactor EntryController

--- src/main/java/com/innova/controller/EntryController.java
+++ src/main/java/com/innova/controller/EntryController.java
@@ -6,8 +6,7 @@ import com.innova.dto.request.LikeForm;
 import com.innova.dto.request.TopicForm;
 import com.innova.dto.response.SuccessResponse;
 import com.innova.exception.BadRequestException;
-import com.innova.model.*;
-import com.innova.repository.*;
+import com.innova.service.EntryService;
 import com.innova.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -16,13 +15,10 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
-import java.time.LocalDateTime;
-import java.util.Date;
 
 @RestController
 @RequestMapping("api/entry")
@@ -32,29 +28,14 @@ public class EntryController {
     UserServiceImpl userServiceImpl;
 
     @Autowired
-    TopicRepository topicRepository;
-
-    @Autowired
-    ContentRepository contentRepository;
-
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    ContentLikeRepository contentLikeRepository;
-
-    @Autowired
-    ContentDislikeRepository contentDislikeRepository;
+    EntryService entryService;
 
     @PostMapping("/addTopic")
     public ResponseEntity<?> addEntry(@Valid @RequestBody TopicForm topicForm) {
-        if (topicRepository.existsByTopicName(topicForm.getTopicName())) {
+        if (entryService.existByTopicNameService(topicForm.getTopicName()))
             throw new BadRequestException("Topic Name is already used!", ErrorCodes.TOPIC_ALREADY_USED);
-        }
-        User user = userServiceImpl.getUserWithAuthentication(SecurityContextHolder.getContext().getAuthentication());
-        LocalDateTime date = LocalDateTime.now();
-        Topic topic = new Topic(topicForm.getTopicName(), 0, user, date);
-        topicRepository.save(topic);
+
+        entryService.addEntryService(topicForm);
 
         SuccessResponse response = new SuccessResponse(HttpStatus.OK, "New Topic added successfully.");
         return new ResponseEntity<>(response, new HttpHeaders(), response.getStatus());
@@ -62,51 +43,38 @@ public class EntryController {
 
     @GetMapping("/getTopics")
     public ResponseEntity<?> getTopic(@PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
-        return ResponseEntity.ok().body(topicRepository.findAllByOrderByCreateDateDesc(pageable));
+        return ResponseEntity.ok().body(entryService.getTopicService(pageable));
     }
 
     @GetMapping("/getLikes/user")
     public ResponseEntity<?> getLikesUser(@RequestParam("id") String contentId, @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
-        if(contentRepository.existsById(Integer.parseInt(contentId))){
-            Content content = contentRepository.findById(Integer.parseInt(contentId));
-            return ResponseEntity.ok().body(contentLikeRepository.findByContent(content,pageable));
-        }
-        else
+        if (!entryService.existByContentIdService(contentId)) {
             throw new BadRequestException("Content is not exist.", ErrorCodes.CONTENT_NOT_VALID);
+        }
+        return ResponseEntity.ok().body(entryService.getLikesUserService(contentId, pageable));
 
     }
+
     @GetMapping("/getDislikes/user")
     public ResponseEntity<?> getDislikesUser(@RequestParam("id") String contentId, @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
-        if(contentRepository.existsById(Integer.parseInt(contentId))){
-            Content content = contentRepository.findById(Integer.parseInt(contentId));
-            return ResponseEntity.ok().body(contentDislikeRepository.findByContent(content,pageable));
-        }
-        else
+        if (!entryService.existByContentIdService(contentId)) {
             throw new BadRequestException("Content is not exist.", ErrorCodes.CONTENT_NOT_VALID);
+        }
+        return ResponseEntity.ok().body(entryService.getDislikesUserService(contentId, pageable));
 
     }
 
     @GetMapping("/getTopics/contentNumber")
     public ResponseEntity<?> getTopicOrderByContent(@PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
-        return ResponseEntity.ok().body(topicRepository.findAllByOrderByContentNumberDesc(pageable));
+        return ResponseEntity.ok().body(entryService.getTopicOrderByContentService(pageable));
     }
 
     @PostMapping("/addContent")
     public ResponseEntity<?> addContent(@Valid @RequestBody ContentForm contentForm) {
-        User user = userServiceImpl.getUserWithAuthentication(SecurityContextHolder.getContext().getAuthentication());
-        LocalDateTime date = LocalDateTime.now();
-
-        if (!topicRepository.existsById(Integer.parseInt(contentForm.getTopicId()))) {
+        if (!entryService.existByTopicIdService(contentForm.getTopicId())) {
             throw new BadRequestException("Topic Name is not valid.", ErrorCodes.TOPIC_NOT_VALID);
         } else {
-            Topic topic = topicRepository.findById(Integer.parseInt(contentForm.getTopicId())).orElseThrow(() -> new BadRequestException("Topic with given topicname could not found", ErrorCodes.TOPIC_NOT_VALID));;
-            Content content = new Content(contentForm.getContent(), 0, 0, 0, 0, date, user, topic);
-            contentRepository.save(content);
-            topic.setContentNumber(topic.getContentNumber() + 1);
-            topic.addCloud_content(content);
-            topicRepository.save(topic);
-            user.addContent(content);
-            userRepository.save(user);
+            entryService.addContentService(contentForm);
             SuccessResponse response = new SuccessResponse(HttpStatus.OK, "New content added successfully.");
             return new ResponseEntity<>(response, new HttpHeaders(), response.getStatus());
         }
@@ -114,29 +82,26 @@ public class EntryController {
 
     @GetMapping("/getContent")
     public ResponseEntity<?> getContent(@RequestParam("id") String topicId, @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
-        if (topicRepository.existsById(Integer.parseInt(topicId))) {
-            Topic topic = topicRepository.findById(Integer.parseInt(topicId)).orElseThrow(() -> new BadRequestException("Topic with given topicname could not found", ErrorCodes.TOPIC_NOT_VALID));;
-            return ResponseEntity.ok().body(contentRepository.findByTopicOrderByDailyLikeDesc(topic, pageable));
+        if (entryService.existByTopicIdService(topicId)) {
+            return ResponseEntity.ok().body(entryService.getContentService(topicId, pageable));
         } else {
             throw new BadRequestException("Topic Name is not valid.", ErrorCodes.TOPIC_NOT_VALID);
         }
     }
 
     @GetMapping("/getTopicName")
-    public ResponseEntity<?> getContent(@RequestParam("id") String topicId ) {
-        if (topicRepository.existsById(Integer.parseInt(topicId))) {
-            Topic topic = topicRepository.findById(Integer.parseInt(topicId)).orElseThrow(() -> new BadRequestException("Topic with given topicname could not found", ErrorCodes.TOPIC_NOT_VALID));;
-            return ResponseEntity.ok().body(topic.getTopicName());
+    public ResponseEntity<?> getTopicName(@RequestParam("id") String topicId) {
+        if (entryService.existByTopicIdService(topicId)) {
+            return ResponseEntity.ok().body(entryService.getTopicNameService(topicId));
         } else {
             throw new BadRequestException("Topic Id is not exist.", ErrorCodes.TOPIC_NOT_VALID);
         }
     }
 
     @GetMapping("/getTopicId")
-    public ResponseEntity<?> getContentWithTopicName(@RequestParam("topicName") String topicName ) {
-        if (topicRepository.existsByTopicName(topicName)) {
-            Topic topic = topicRepository.findByTopicName(topicName).orElseThrow(() -> new BadRequestException("Topic with given topicname could not found", ErrorCodes.TOPIC_NOT_VALID));;
-            return ResponseEntity.ok().body(topic.getId());
+    public ResponseEntity<?> getContentWithTopicName(@RequestParam("topicName") String topicName) {
+        if (entryService.existByTopicNameService(topicName)) {
+            return ResponseEntity.ok().body(entryService.getTopicIdService(topicName));
         } else {
             throw new BadRequestException("Topic Name is not exist.", ErrorCodes.TOPIC_NOT_VALID);
         }
@@ -144,68 +109,36 @@ public class EntryController {
 
     @GetMapping("/getContent/random")
     public ResponseEntity<?> getContentOrderByLike(@PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
-            return ResponseEntity.ok().body(contentRepository.findByOrderByLikeDesc(pageable));
+        return ResponseEntity.ok().body(entryService.getContentOrderByLikeService(pageable));
     }
 
     @GetMapping("/getMyContents")
     public ResponseEntity<?> getMyContents(@PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
-        User user = userServiceImpl.getUserWithAuthentication(SecurityContextHolder.getContext().getAuthentication());
-        return ResponseEntity.ok().body(contentRepository.findByUserOrderByCreateDateDesc(user, pageable));
+        return ResponseEntity.ok().body(entryService.getMyContentsService(pageable));
     }
 
     @GetMapping("/getUserContents")
     public ResponseEntity<?> getUserContents(@RequestParam("userName") String userName, @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
-        User user = userRepository.findByUsername(userName)
-                .orElseThrow(() -> new BadRequestException("User with given username could not found", ErrorCodes.NO_SUCH_USER));
-        return ResponseEntity.ok().body(contentRepository.findByUserOrderByCreateDateDesc(user, pageable));
+        return ResponseEntity.ok().body(entryService.getUserContentsService(userName, pageable));
     }
 
     @PutMapping("/like-dislike")
     @Transactional
     public ResponseEntity<?> likeDislike(@Valid @RequestBody LikeForm likeForm) {
-        User user = userServiceImpl.getUserWithAuthentication(SecurityContextHolder.getContext().getAuthentication());
-        if (contentRepository.existsById(Integer.parseInt(likeForm.getContentID()))) {
-            Content content = contentRepository.findById(Integer.parseInt(likeForm.getContentID()));
-            if (!contentLikeRepository.existsByUserAndContent(user, content) && !contentDislikeRepository.existsByUserAndContent(user, content)) {
-                if (likeForm.getLike().equals("like")) {
-                    content.setLike(content.getLike() + 1);
-                    content.setDailyLike(content.getDailyLike() + 1);
-                    contentRepository.save(content);
-                    contentLikeRepository.save(new ContentLike(user, content));
-                    SuccessResponse response = new SuccessResponse(HttpStatus.OK, "Successfully liked.");
-                    return new ResponseEntity<>(response, new HttpHeaders(), response.getStatus());
-                } else if (likeForm.getLike().equals("dislike")) {
-                    content.setDislike(content.getDislike() + 1);
-                    content.setDailyDislike(content.getDailyDislike() + 1);
-                    contentRepository.save(content);
-                    contentDislikeRepository.save(new ContentDislike(user, content));
-                    SuccessResponse response = new SuccessResponse(HttpStatus.OK, "Successfully disliked.");
-                    return new ResponseEntity<>(response, new HttpHeaders(), response.getStatus());
-                } else {
-                    throw new BadRequestException("Something is wrong", ErrorCodes.SOMETHING_IS_WRONG);
-                }
-            } else if (contentLikeRepository.existsByUserAndContent(user, content) && !contentDislikeRepository.existsByUserAndContent(user, content) && likeForm.getLike().equals("cancel-like")) {
-                content.setLike(content.getLike() - 1);
-                content.setDailyLike(content.getDailyLike() - 1);
-                contentRepository.save(content);
-                contentLikeRepository.delete(contentLikeRepository.findByUserAndContent(user, content));
-                SuccessResponse response = new SuccessResponse(HttpStatus.OK, "Successfully cancelled.");
-                return new ResponseEntity<>(response, new HttpHeaders(), response.getStatus());
-            } else if (!contentLikeRepository.existsByUserAndContent(user, content) && contentDislikeRepository.existsByUserAndContent(user, content) && likeForm.getLike().equals("cancel-dislike")) {
-                content.setDislike(content.getDislike() - 1);
-                content.setDailyDislike(content.getDailyDislike() - 1);
-                contentRepository.save(content);
-                contentDislikeRepository.delete(contentDislikeRepository.findByUserAndContent(user, content));
-                SuccessResponse response = new SuccessResponse(HttpStatus.OK, "Successfully cancelled.");
-                return new ResponseEntity<>(response, new HttpHeaders(), response.getStatus());
-            } else {
+        if (entryService.existByContentIdService(likeForm.getContentID())) {
+            String reMessage = entryService.likeDislikeService(likeForm);
+            if (reMessage.equals("Hata")) {
                 if (likeForm.getLike().equals("like"))
                     throw new BadRequestException("Content already liked.", ErrorCodes.CONTENT_ALREADY_LIKED);
                 else if (likeForm.getLike().equals("dislike"))
                     throw new BadRequestException("Content already disliked.", ErrorCodes.CONTENT_ALREADY_DISLIKED);
                 else
                     throw new BadRequestException("Something is wrong", ErrorCodes.SOMETHING_IS_WRONG);
+            } else {
+                SuccessResponse response = new SuccessResponse(HttpStatus.OK, reMessage);
+                return new ResponseEntity<>(response, new HttpHeaders(), response.getStatus());
             }
+
         } else {
             throw new BadRequestException("Content is not valid.", ErrorCodes.CONTENT_NOT_VALID);
 
@@ -214,12 +147,11 @@ public class EntryController {
 
     @GetMapping("/getLikes")
     public ResponseEntity<?> getMyLikes(@RequestParam("like") String like, @RequestParam("userName") String userName, @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
-        User user = userRepository.findByUsername(userName)
-                .orElseThrow(() -> new BadRequestException("User with given username could not found", ErrorCodes.NO_SUCH_USER));
+
         if (like.equals("like")) {
-            return ResponseEntity.ok().body(contentLikeRepository.findByUser(user, pageable));
+            return ResponseEntity.ok().body(entryService.getMyLikesService(userName, pageable));
         } else if (like.equals("dislike")) {
-            return ResponseEntity.ok().body(contentDislikeRepository.findByUser(user, pageable));
+            return ResponseEntity.ok().body(entryService.getMyDislikesService(userName, pageable));
         } else {
             throw new BadRequestException("Something is wrong", ErrorCodes.SOMETHING_IS_WRONG);
         }
@@ -227,20 +159,10 @@ public class EntryController {
 
     @DeleteMapping("/deleteContent")
     @Transactional
-    public ResponseEntity<?> deleteContent(@RequestParam("contentId") String contentId, @RequestParam("topicName") String topicName) {
-        User user = userServiceImpl.getUserWithAuthentication(SecurityContextHolder.getContext().getAuthentication());
-        Topic topic = topicRepository.findByTopicName(topicName).orElseThrow(() -> new BadRequestException("Topic with given topicname could not found", ErrorCodes.TOPIC_NOT_VALID));;
-        Content content = contentRepository.findById(Integer.parseInt(contentId));
-        if (contentLikeRepository.existsByUserAndContent(user, content))
-            contentLikeRepository.delete(contentLikeRepository.findByUserAndContent(user, content));
-        if (contentDislikeRepository.existsByUserAndContent(user, content))
-            contentDislikeRepository.delete(contentDislikeRepository.findByUserAndContent(user, content));
-        topic.setContentNumber(topic.getContentNumber() - 1);
-        topic.removeCloud_content(content);
-        user.removeContent(content);
-        topicRepository.save(topic);
-        userRepository.save(user);
-        contentRepository.delete(content);
+    public ResponseEntity<?> deleteContent(@RequestParam("contentId") String contentId, @RequestParam("topicId") String topicId) {
+        if (!entryService.existByTopicIdService(topicId))
+            throw new BadRequestException("Topic with given topicId could not found", ErrorCodes.TOPIC_NOT_VALID);
+        entryService.deleteContentService(contentId, topicId);
         SuccessResponse response = new SuccessResponse(HttpStatus.OK, "Content removed successfully");
         return new ResponseEntity<>(response, new HttpHeaders(), response.getStatus());
     }
---
+++ src/main/java/com/innova/service/EntryService.java
@@ -0,0 +1,194 @@
+package com.innova.service;
+
+import com.innova.constants.ErrorCodes;
+import com.innova.dto.request.ContentForm;
+import com.innova.dto.request.LikeForm;
+import com.innova.dto.request.TopicForm;
+import com.innova.exception.BadRequestException;
+import com.innova.model.*;
+import com.innova.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class EntryService {
+
+    @Autowired
+    TopicRepository topicRepository;
+
+    @Autowired
+    ContentRepository contentRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    ContentLikeRepository contentLikeRepository;
+
+    @Autowired
+    ContentDislikeRepository contentDislikeRepository;
+
+    @Autowired
+    UserServiceImpl userServiceImpl;
+
+    public Page<Topic> getTopicService(Pageable pageable) {
+        return topicRepository.findAllByOrderByCreateDateDesc(pageable);
+    }
+
+    public void addEntryService(TopicForm topicForm) {
+        User user = userServiceImpl.getUserWithAuthentication(SecurityContextHolder.getContext().getAuthentication());
+        LocalDateTime date = LocalDateTime.now();
+        Topic topic = new Topic(topicForm.getTopicName(), 0, user, date);
+        topicRepository.save(topic);
+    }
+
+    public boolean existByTopicNameService(String topicName) {
+        if (topicRepository.existsByTopicName(topicName)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean existByTopicIdService(String id) {
+        if (topicRepository.existsById(Integer.parseInt(id))) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean existByContentIdService(String contentId) {
+        if (contentRepository.existsById(Integer.parseInt(contentId))) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public Page<ContentLike> getLikesUserService(String contentId, Pageable pageable) {
+        Content content = contentRepository.findById(Integer.parseInt(contentId));
+        return contentLikeRepository.findByContent(content, pageable);
+    }
+
+    public Page<ContentLike> getDislikesUserService(String contentId, Pageable pageable) {
+        Content content = contentRepository.findById(Integer.parseInt(contentId));
+        return contentLikeRepository.findByContent(content, pageable);
+    }
+
+    public Page<Topic> getTopicOrderByContentService(Pageable pageable) {
+        return topicRepository.findAllByOrderByContentNumberDesc(pageable);
+    }
+
+    public void addContentService(ContentForm contentForm) {
+        User user = userServiceImpl.getUserWithAuthentication(SecurityContextHolder.getContext().getAuthentication());
+        LocalDateTime date = LocalDateTime.now();
+        Topic topic = topicRepository.findById(Integer.parseInt(contentForm.getTopicId())).orElseThrow(() -> new BadRequestException("Topic with given topicname could not found", ErrorCodes.TOPIC_NOT_VALID));
+        ;
+        Content content = new Content(contentForm.getContent(), 0, 0, 0, 0, date, user, topic);
+        contentRepository.save(content);
+        topic.setContentNumber(topic.getContentNumber() + 1);
+        topic.addCloud_content(content);
+        topicRepository.save(topic);
+        user.addContent(content);
+        userRepository.save(user);
+    }
+
+    public Page<Content> getContentService(String topicId, Pageable pageable) {
+        Topic topic = topicRepository.findById(Integer.parseInt(topicId)).orElseThrow(() -> new BadRequestException("Topic with given topicname could not found", ErrorCodes.TOPIC_NOT_VALID));
+        return contentRepository.findByTopicOrderByDailyLikeDesc(topic, pageable);
+    }
+
+    public String getTopicNameService(String topicId) {
+        Topic topic = topicRepository.findById(Integer.parseInt(topicId)).orElseThrow(() -> new BadRequestException("Topic with given topicname could not found", ErrorCodes.TOPIC_NOT_VALID));
+        ;
+        return (topic.getTopicName());
+    }
+
+    public String getTopicIdService(String topicName) {
+        Topic topic = topicRepository.findByTopicName(topicName).orElseThrow(() -> new BadRequestException("Topic with given topicname could not found", ErrorCodes.TOPIC_NOT_VALID));
+        ;
+        return (topic.getTopicName());
+    }
+
+    public Page<Content> getContentOrderByLikeService(Pageable pageable) {
+        return contentRepository.findByOrderByLikeDesc(pageable);
+    }
+
+    public Page<Content> getMyContentsService(Pageable pageable) {
+        User user = userServiceImpl.getUserWithAuthentication(SecurityContextHolder.getContext().getAuthentication());
+        return contentRepository.findByUserOrderByCreateDateDesc(user, pageable);
+    }
+
+    public Page<Content> getUserContentsService(String userName, Pageable pageable) {
+        User user = userRepository.findByUsername(userName)
+                .orElseThrow(() -> new BadRequestException("User with given username could not found", ErrorCodes.NO_SUCH_USER));
+        return contentRepository.findByUserOrderByCreateDateDesc(user, pageable);
+    }
+
+    public Page<ContentLike> getMyLikesService(String userName, Pageable pageable) {
+        User user = userRepository.findByUsername(userName)
+                .orElseThrow(() -> new BadRequestException("User with given username could not found", ErrorCodes.NO_SUCH_USER));
+        return contentLikeRepository.findByUser(user, pageable);
+    }
+
+    public Page<ContentDislike> getMyDislikesService(String userName, Pageable pageable) {
+        User user = userRepository.findByUsername(userName)
+                .orElseThrow(() -> new BadRequestException("User with given username could not found", ErrorCodes.NO_SUCH_USER));
+        return contentDislikeRepository.findByUser(user, pageable);
+    }
+
+    public void deleteContentService(String contentId, String topicId) {
+        User user = userServiceImpl.getUserWithAuthentication(SecurityContextHolder.getContext().getAuthentication());
+        Topic topic = topicRepository.findById(Integer.parseInt(topicId)).orElseThrow(() -> new BadRequestException("Topic with given topicId could not found", ErrorCodes.TOPIC_NOT_VALID));
+        Content content = contentRepository.findById(Integer.parseInt(contentId));
+        if (contentLikeRepository.existsByUserAndContent(user, content))
+            contentLikeRepository.delete(contentLikeRepository.findByUserAndContent(user, content));
+        if (contentDislikeRepository.existsByUserAndContent(user, content))
+            contentDislikeRepository.delete(contentDislikeRepository.findByUserAndContent(user, content));
+        topic.setContentNumber(topic.getContentNumber() - 1);
+        topic.removeCloud_content(content);
+        user.removeContent(content);
+        topicRepository.save(topic);
+        userRepository.save(user);
+        contentRepository.delete(content);
+    }
+
+
+    public String likeDislikeService(LikeForm likeForm) {
+        User user = userServiceImpl.getUserWithAuthentication(SecurityContextHolder.getContext().getAuthentication());
+        Content content = contentRepository.findById(Integer.parseInt(likeForm.getContentID()));
+        if (!contentLikeRepository.existsByUserAndContent(user, content) && !contentDislikeRepository.existsByUserAndContent(user, content)) {
+            if (likeForm.getLike().equals("like")) {
+                content.setLike(content.getLike() + 1);
+                content.setDailyLike(content.getDailyLike() + 1);
+                contentRepository.save(content);
+                contentLikeRepository.save(new ContentLike(user, content));
+                return "Successfully liked.";
+
+            } else if (likeForm.getLike().equals("dislike")) {
+                content.setDislike(content.getDislike() + 1);
+                content.setDailyDislike(content.getDailyDislike() + 1);
+                contentRepository.save(content);
+                contentDislikeRepository.save(new ContentDislike(user, content));
+                return "Successfully disliked.";
+            }
+        } else if (contentLikeRepository.existsByUserAndContent(user, content) && !contentDislikeRepository.existsByUserAndContent(user, content) && likeForm.getLike().equals("cancel-like")) {
+            content.setLike(content.getLike() - 1);
+            content.setDailyLike(content.getDailyLike() - 1);
+            contentRepository.save(content);
+            contentLikeRepository.delete(contentLikeRepository.findByUserAndContent(user, content));
+            return "Successfully cancelled.";
+        } else if (!contentLikeRepository.existsByUserAndContent(user, content) && contentDislikeRepository.existsByUserAndContent(user, content) && likeForm.getLike().equals("cancel-dislike")) {
+            content.setDislike(content.getDislike() - 1);
+            content.setDailyDislike(content.getDailyDislike() - 1);
+            contentRepository.save(content);
+            contentDislikeRepository.delete(contentDislikeRepository.findByUserAndContent(user, content));
+            return "Successfully cancelled.";
+        }
+        return "Hata";
+    }
+}
